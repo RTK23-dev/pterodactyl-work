71053b9f31c873f8d901ae4e409ece9bd963ec9f
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-09-26 15:19:52 +0530

    Update autoinstall.sh

--- JustAtheme/autoinstall.sh
+++ JustAtheme/autoinstall.sh
@@ -1,1 +1,14 @@
-
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl
+rm -rf tailwind.config.js
+wget https://raw.githubusercontent.com/fRTK23-dev/pterodactyl-work/main/JustAtheme/autoinstall.sh
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt install -y nodejs
+npm i -g yarn
+cd /var/www/pterodactyl
+yarn install
+yarn add @emotion/react
+yarn build:production
+clear
+php /var/www/pterodactyl/artisan up
+echo "Done Check up your panel!"
