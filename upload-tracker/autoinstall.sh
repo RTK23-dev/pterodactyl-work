064b3c5bd17748ee1e4c09c7765596fb1b1987ea
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-09-26 20:10:52 +0530

    Update autoinstall.sh

--- upload-tracker/autoinstall.sh
+++ upload-tracker/autoinstall.sh
@@ -1,8 +1,7 @@
 php /var/www/pterodactyl/artisan down
 /var/www/pterodactyl/resources/scripts/components/server/files
 rm -rf UploadButton.tsx
-wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/
-upload-tracker/UploadButton.tsx
+wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/upload-tracker/UploadButton.tsx
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt install -y nodejs
 npm i -g yarn
