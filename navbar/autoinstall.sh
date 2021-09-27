5b3c9af452544e20127e7f48ff2f4e8a05bfd56f
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-09-27 16:36:35 +0530

    Update autoinstall.sh

--- navbar/autoinstall.sh
+++ navbar/autoinstall.sh
@@ -1,6 +1,6 @@
-cd /var/www/pterodactyl/resources/scripts/components/
+cd /var/www/pterodactyl/resources/scripts/components
 rm -rf NavigationBar.tsx
-wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/navbar/Navigationbar.tsx
+wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/navbar/NavigationBar.tsx
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 apt install -y nodejs
 npm i -g yarn
