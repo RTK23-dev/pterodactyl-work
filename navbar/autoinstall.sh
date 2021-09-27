8ebe54316f586ff3c50d0da80bc7c4f770add054
RTK23-dev <77203747+RTK23-dev@users.noreply.github.com>
2021-09-27 16:43:01 +0530

    Update autoinstall.sh

--- navbar/autoinstall.sh
+++ navbar/autoinstall.sh
@@ -1,3 +1,4 @@
+php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts/components
 rm -rf NavigationBar.tsx
 wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/navbar/NavigationBar.tsx
@@ -8,5 +9,6 @@ cd /var/www/pterodactyl
 yarn install
 yarn add @emotion/react
 yarn build:production
+php /var/www/pterodactyl/artisan up
 clear
 echo "Done theme has been installed thanks for using this script make sure to star the repo!"
