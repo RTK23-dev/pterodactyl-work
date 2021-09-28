04f6cc3992b1f043a292b39501a9ba147388fd0f
Ayush <77203747+RTK23-dev@users.noreply.github.com>
2021-09-28 19:42:14 +0530

    Create autoinstall.sh

---
+++ big-serverRow/autoinstall.sh
@@ -0,0 +1,14 @@
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl/resources/scripts/components/dashboard
+rm -rf ServerRow.tsx
+wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/big-serverRow/ServerRow.tsx
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+apt install -y nodejs
+npm i -g yarn
+cd /var/www/pterodactyl
+yarn install
+yarn add @emotion/react
+yarn build:production
+php /var/www/pterodactyl/artisan up
+clear
+echo "Done addon has been installed thanks for using this script make sure to star the repo!"
