d96b82585312958da790ba089a7dc4acee0fd92a
Ayush <77203747+RTK23-dev@users.noreply.github.com>
2021-09-29 09:31:54 +0530

    Update autoinstall.sh

--- whitelight/autoinstall.sh
+++ whitelight/autoinstall.sh
@@ -2,7 +2,7 @@ php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl
 rm -rf tailwind.config.js
 wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/whitelight/tailwind.config.js
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt install -y nodejs
 npm i -g yarn
 cd /var/www/pterodactyl
@@ -11,4 +11,4 @@ yarn add @emotion/react
 yarn build:production
 php /var/www/pterodactyl/artisan up
 clear
-echo "Done theme has been installed thanks for using this theme / script make sure to star the repo!"
+echo "Done theme has been installed thanks for using this script make sure to star the repo!"
