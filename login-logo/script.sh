be5f19e911273833ab9eeff4eec3f3eb0a8f33bc
Ayush <77203747+RTK23-dev@users.noreply.github.com>
2021-09-30 10:10:53 +0530

    Update script.sh

--- login-logo/script.sh
+++ login-logo/script.sh
@@ -1,1 +1,14 @@
-
+php /var/www/pterodactyl/artisan down
+cd /var/www/pterodactyl/resources/scripts/components/auth
+rm -rf LoginFromContainer.tsx
+wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/login-logo/LoginFromContainer.tsx
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt install -y nodejs
+npm i -g yarn
+cd /var/www/pterodactyl
+yarn install
+yarn add @emotion/react
+yarn build:production
+php /var/www/pterodactyl/artisan up
+clear
+echo "Done"
