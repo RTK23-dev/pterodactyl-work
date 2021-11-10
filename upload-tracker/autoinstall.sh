e69f52d4cd0e1fb80a8455f083f9a639c04fd49f
Ayush <77203747+RTK23-dev@users.noreply.github.com>
2021-11-10 08:16:10 +0530

    Update autoinstall.sh

--- upload-tracker/autoinstall.sh
+++ upload-tracker/autoinstall.sh
@@ -2,7 +2,7 @@ php /var/www/pterodactyl/artisan down
 cd /var/www/pterodactyl/resources/scripts/components/server/files
 rm -rf UploadButton.tsx
 wget https://raw.githubusercontent.com/RTK23-dev/pterodactyl-work/main/upload-tracker/UploadButton.tsx
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 apt install -y nodejs
 npm i -g yarn
 cd /var/www/pterodactyl
@@ -11,4 +11,4 @@ yarn add @emotion/react
 yarn build:production
 php /var/www/pterodactyl/artisan up
 clear
-echo "Done addon has been installed thanks for using this theme / script make sure to star the repo!"
+echo "Done addon has been installed thanks for using this script make sure to star the repo!"
